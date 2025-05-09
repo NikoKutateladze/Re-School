d7885883153e95d02c564e879aed77eff4b80162
NikoKutateladze <140888630+NikoKutateladze@users.noreply.github.com>
2025-05-09 12:26:58 +0400

    Class exam (May)

--- KeyAutoClicker/AutoClicker.py
+++ KeyAutoClicker/AutoClicker.py
@@ -1,10 +1,17 @@
-import keyboard
+import pydirectinput
 import time
+import keyboard
 
 def press_space():
-    while True:
-        keyboard.press('space')
-        keyboard.release('space')
-        time.sleep(2)
+    try:
+        while not keyboard.is_pressed('`'):  # Stop with backtick (`) key
+            pydirectinput.press('space')  # Jump
+            time.sleep(4)
+            pydirectinput.press('w')  # Move forward
+            time.sleep(3)
+            pydirectinput.press('s')  # Move backward
+            time.sleep(4)
+    except KeyboardInterrupt:
+        print("\nScript stopped by user.")
 
-press_space() 
+press_space()
